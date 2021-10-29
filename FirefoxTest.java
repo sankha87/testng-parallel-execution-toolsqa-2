8b9ea0e29942fabb59cf27630cfff620bc21028d
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2021-10-29 18:26:46 +0530

    Create FirefoxTest.java

---
+++ FirefoxTest.java
@@ -0,0 +1,38 @@
+package classes;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirefoxTest {
+
+	public WebDriver driver; 
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Initilizing the Mozilla Firefox Driver");
+System.setProperty("webdriver.gecko.driver", "C:\\browser drivers\\geckodriver.exe");		
+		driver = new FirefoxDriver();
+	}
+        @Test
+ 	public void FirefoxTestMethod()
+ 	{ 
+	  //Initialize the firefox driver
+	 System.out.println("The thread ID for Firefox is "+ Thread.currentThread().getId());
+	 driver.get("https://www.demoqa.com"); 
+	 WebElement e = driver.findElement(By.xpath("(//div[@class='card-up'])[1]"));
+		Actions act = new Actions(driver);
+		act.moveToElement(e).perform();
+		act.click(e).perform(); 
+    }
+ 
+     @AfterTest
+     public void afterTest() {
+	 System.out.println("Closing the browser ");
+//	 driver.close();
+     }
+}
